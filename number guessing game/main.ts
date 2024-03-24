1fb9e868cf352c75424f839235aceab725bc5f0c
Khudaja <163579620+Khudaja@users.noreply.github.com>
2024-03-24 19:53:58 +0500

    Add files via upload

---
+++ number guessing game/main.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const computerchoice = Math.floor(Math.random() * 6+1)
+let numberofguess = 4
+while (numberofguess > 0){
+    let answer = await inquirer.prompt([{
+        name : "userguess",
+        type: "number",
+        message : "guess your number between 1 to 6",},
+    ]);
+
+    if (answer. userguess == computerchoice ){
+        console.log("you won!!")
+        break;
+    }else {
+        console.log("you lost!!")
+    }
+    numberofguess--
+}
+if (numberofguess == 0){
+    console.log(`you guessed wrong the correct guess was ${computerchoice}`)
+}
