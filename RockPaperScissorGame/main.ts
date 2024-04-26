1929ace8944ebcaa289622340b11e4f3bb75adbd
Khudaja <163579620+Khudaja@users.noreply.github.com>
2024-04-26 22:12:05 +0500

    Add files via upload

---
+++ RockPaperScissorGame/main.ts
@@ -0,0 +1,27 @@
+#! usr/bin/env node
+
+import inquirer from "inquirer";
+const choices = ["Rock", "Paper", "Scissor"];
+const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+let answer = await inquirer.prompt(
+    [
+    {
+    name : "input",
+    type : "list",
+    choices : ["Rock", "Paper" , "Scissor"],
+    messsage : "enter your choice",
+    },
+    ]
+    );
+console.log(`computers choice ${computerChoice}`);
+if (( computerChoice == "rock" && answer.input == "scissor")||
+   ( computerChoice == "paper" && answer.input == "rock")||
+   ( computerChoice == "scissor" && answer.input == "paper")){
+    console.log("computer won the game!")
+   } else if (computerChoice == answer.input){
+    console.log("its a tie")
+   } else {
+    console.log("hurray you won the game!!")
+   }
+
